f7440d6a264f9e8559feaa438e6b56b1fb298548
elvlalad <elvalad@gmail.com>
2015-12-18 15:23:25 +0800

    修改Bug。

--- SeetongNew/src/com/seetong5/app/seetong/ui/MainActivity2.java
+++ SeetongNew/src/com/seetong5/app/seetong/ui/MainActivity2.java
@@ -564,7 +564,7 @@ public class MainActivity2 extends BaseActivity {
         }*/
     }
 
-    private void saveData() {
+    public void saveData() {
         Global.m_spu.saveSharedPreferences(Define.EXIT_APP_NORMALLY, true);
         boolean bSaveData = Global.m_spu.saveSharedPreferences(Define.IS_SAVE_DEV_LIST, true);
         //Log.d(TAG, "save device order : " + bSaveData);
