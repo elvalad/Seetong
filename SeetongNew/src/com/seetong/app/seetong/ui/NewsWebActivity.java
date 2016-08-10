f544f9c3811769c24e4be095b4dcbbb4d959d645
elvlalad <elvalad@gmail.com>
2016-08-10 09:54:20 +0800

    修改微信公众平台AppID用于微信分享。

--- SeetongNew/src/com/seetong/app/seetong/ui/NewsWebActivity.java
+++ SeetongNew/src/com/seetong/app/seetong/ui/NewsWebActivity.java
@@ -219,13 +219,13 @@ public class NewsWebActivity extends BaseActivity {
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
         oks.setTitleUrl(newsUrl);
         // text是分享文本，所有平台都需要这个字段
-        oks.setText(newsTitle);
+        oks.setText(newsTitle + "\n" +newsUrl);
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
         oks.setUrl(newsUrl);
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        //oks.setComment("我是测试评论文本");
+        oks.setComment(newsTitle + newsUrl);
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
